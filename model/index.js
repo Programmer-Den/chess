b3074940f29bbfb97047183bc0309d11b1a52b4a
Programmer-Den <denis.from.grodno@gmail.com>
2023-04-11 01:25:15 +0300

    Write permitMove, rules for Queen & Rook & Bishop

--- model/index.js
+++ model/index.js
@@ -14,14 +14,57 @@ turn();
 
 function turn() {
   while (!checkmate) {
-    move('White');
-    move('Black');
+    move('White');          if (checkmate) break;
+    move('Black')
   }
-
   let winner = Object.values(board.currentBoard).includes('♚') ? 'White' : 'Black';
 
   console.log(`${winner} win! Congratulations.`);
 }
+
+function defineChessmanKind(chessman) {
+  switch (chessman) {
+    case '♚':
+    case '♔':
+      return 'King';
+
+    case '♛':
+    case '♕':
+      return 'Queen';
+
+    case '♜':
+    case '♖':
+      return 'Rook';
+
+    case '♝':
+    case '♗':
+      return 'Bishop';
+
+    case '♞':
+    case '♘':
+      return 'Knight';
+    
+    case '♟':  return 'whitePawn'; 
+    case '♙':  return 'blackPawn'
+  }
+}
+function permitMove(chessmanKind, FROM_cell, ONTO_cell) {
+  switch (chessmanKind) {
+    case 'King':     King()(FROM_cell, ONTO_cell); break;
+    case 'Queen':   Queen()(FROM_cell, ONTO_cell); break;
+    case 'Rook':     Rook()(FROM_cell, ONTO_cell); break;
+    case 'Bishop': Bishop()(FROM_cell, ONTO_cell); break;
+    case 'Knight': Knight()(FROM_cell, ONTO_cell); break;
+
+    case 'whitePawn': Pawn('white')(FROM_cell, ONTO_cell); break;
+    case 'blackPawn': Pawn('black')(FROM_cell, ONTO_cell)
+  }
+}
+function Queen() {
+  return function isMoveAvailable(FROM_cell, ONTO_cell) {
+    return Rook()(FROM_cell, ONTO_cell) || Bishop()(FROM_cell, ONTO_cell)
+  }
+}  //  other chessmen rules reside at the bottom of this file
 
 function move(color) {
   console.log(board.boardDisplay() );
@@ -29,11 +72,11 @@ function move(color) {
   let FROM = prompt(`${color} turn! Enter cell 'FROM': `).toUpperCase();
   let ONTO = prompt(`${color} turn. Enter cell 'ONTO': `).toUpperCase();
   
-  prompt.history.save();
+  prompt.history.save();  //  the prompt history being saved in the .prompt_hist.txt file
 
   // First White chessman: 9818 in Unicode    Last White chessman: 9823 in Unicode
   // First Black chessman: 9812 in Unicode    Last Black chessman: 9817 in Unicode
-  
+
   if (  !(FROM in board.currentBoard)  ||  !(ONTO in board.currentBoard)  ||
 
       (color == 'White' && (board.currentBoard[FROM] <  String.fromCodePoint(9818)     ||
@@ -43,11 +86,13 @@ function move(color) {
       (color == 'Black' && (board.currentBoard[ONTO] <= String.fromCodePoint(9817)     &&
                             board.currentBoard[ONTO] >= String.fromCodePoint(9812) ) ) ||
       (color == 'Black' && (board.currentBoard[FROM] <  String.fromCodePoint(9812)     ||
-                            board.currentBoard[FROM] >  String.fromCodePoint(9817) ) )  )
+                            board.currentBoard[FROM] >  String.fromCodePoint(9817) ) ) ||
+      
+      !permitMove(defineChessmanKind(board.currentBoard[FROM]), FROM, ONTO)  )
   {
     console.log('Incorrect input... Please, enter correct cells');
     
-    return move(color);
+    return move(color)
   }
 
   let chessman = board.currentBoard[FROM];
@@ -55,6 +100,7 @@ function move(color) {
   board.currentBoard[FROM] = ' ';
   board.currentBoard[ONTO] = chessman;
 
+  // castling (20 lines: from 104 to 123)
   if (board.castlingOption[color] && ( (FROM == 'E8' && (ONTO == 'C8' || ONTO == 'G8') ) ||
                                        (FROM == 'E1' && (ONTO == 'C1' || ONTO == 'G1') ) ) )
   {
@@ -75,6 +121,110 @@ function move(color) {
   {
     board.castlingOption[color] = false
   }
-  
+
   console.log(board.boardDisplay() );
+}
+
+function King() {
+  return function isMoveAvailable(FROM_cell, ONTO_cell) {
+    
+  }
+}
+
+function Rook() {
+  return function isMoveAvailable(FROM_cell, ONTO_cell) {
+    if (FROM_cell[0] === ONTO_cell[0]) {    //   vertical
+      if (+FROM_cell[1] < +ONTO_cell[1]) {   //   upward
+        for (let i = +FROM_cell[1] + 1;
+                 i < +ONTO_cell[1];
+                 i++)
+        {
+          if (board.currentBoard[`${FROM_cell[0]}${i}`] !== ' ') return false;
+        }
+        return true
+      }
+      //                                 vertical downward
+        for (let i = +FROM_cell[1] - 1;  
+                 i > +ONTO_cell[1];
+                 i--)
+        {
+          if (board.currentBoard[`${FROM_cell[0]}${i}`] !== ' ') return false;
+        }
+        return true
+    }
+    //                                            horizontal
+      if (FROM_cell[0] < ONTO_cell[0]) {    //     rightward
+        for (let i = FROM_cell.codePointAt() + 1;
+                 i < ONTO_cell.codePointAt();
+                 i++)
+        {
+          if (board.currentBoard[`${String.fromCodePoint(i)}${FROM_cell[1]}`] !== ' ')
+                                                                               return false;
+        }
+        return true
+      }
+      //                                           horizontal leftward
+        for (let i = FROM_cell.codePointAt() - 1;
+                 i > ONTO_cell.codePointAt();
+                 i--)
+        {
+          if (board.currentBoard[`${String.fromCodePoint(i)}${FROM_cell[1]}`] !== ' ')
+                                                                               return false;
+        }
+        return true
+  }
+}
+
+function Bishop() {
+  return function isMoveAvailable(FROM_cell, ONTO_cell) {
+    if (FROM_cell[0] < ONTO_cell[0]) {      //                           rightward
+      if (FROM_cell[1] < ONTO_cell[1]) {   //                               upward
+        for (let l = FROM_cell.codePointAt() + 1, d = +FROM_cell[1] + 1;
+                 l < ONTO_cell.codePointAt(),     d < +ONTO_cell[1];
+                 l++,  /* letter       digit */   d++)
+        {
+          if (board.currentBoard[`${String.fromCodePoint(l)}${d}`] !== ' ') return false;
+        }
+        return true
+      }
+      //                                                                  rightward downward
+        for (let l = FROM_cell.codePointAt() + 1, d = +FROM_cell[1] - 1;
+                 l < ONTO_cell.codePointAt(),     d > +ONTO_cell[1];
+                 l++,  /* letter       digit */   d--)
+        {
+          if (board.currentBoard[`${String.fromCodePoint(l)}${d}`] !== ' ') return false;
+        }
+        return true
+    }
+    //                                                                    leftward
+      if (FROM_cell[1] < ONTO_cell[1]) {   //                             downward
+        for (let l = FROM_cell.codePointAt() - 1, d = +FROM_cell[1] - 1;
+                 l > ONTO_cell.codePointAt(),     d > +ONTO_cell[1];
+                 l--,  /* letter       digit */   d--)
+        {
+          if (board.currentBoard[`${String.fromCodePoint(l)}${d}`] !== ' ') return false;
+        }
+        return true
+      }
+      //                                                                  leftward upward
+        for (let l = FROM_cell.codePointAt() - 1, d = +FROM_cell[1] + 1;
+                 l > ONTO_cell.codePointAt(),     d < +ONTO_cell[1];
+                 l--,  /* letter       digit */   d++)
+        {
+          if (board.currentBoard[`${String.fromCodePoint(l)}${d}`] !== ' ') return false;
+        }
+        return true
+  }
+}
+
+function Knight() {
+  return function isMoveAvailable(FROM_cell, ONTO_cell) {
+    
+  }
+}
+
+function Pawn(color) {
+  return function isMoveAvailable(FROM_cell, ONTO_cell) {
+    
+  }
 }
